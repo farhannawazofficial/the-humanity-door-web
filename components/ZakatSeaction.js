85fa082379ed78ca3ec0d40190580db6ee4bd607
Farhan Nawaz <farhannawazofficial@gmail.com>
2025-01-15 13:13:42 +0500

    Update ZakatSeaction.js

--- components/ZakatSeaction.js
+++ components/ZakatSeaction.js
@@ -34,7 +34,7 @@ export default function ZakatSection() {
               ].map((category, index) => (
                 <button
                   key={index}
-                  className="px-4 py-2 text-sm font-medium bg-white text-gray-700 border border-gray-300 rounded-md shadow-md hover:bg-gray-100 transition-all"
+                  className="px-4 py-2 text-sm font-medium bg-white text-gray-700 border border-gray-300 rounded-md shadow-md hover:bg-gray-900 hover:text-white transition-all"
                 >
                   {category}
                 </button>
